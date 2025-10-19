1e832be65dcba5a6279574a95ed1ee63658ec69e
mat0wa3 <mmateo1804@gmail.com>
2025-10-18 23:47:21 -0500

    Change App class export to default for consistency

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 
-export class App {
+export default class App {
     constructor() {
         this.app = express()
         this.port = process.env.PORT || 3000
