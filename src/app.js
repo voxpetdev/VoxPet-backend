d294c45759cc191b7d59c9f3ebd1ec26b4160e1a
mat0wa3 <mmateo1804@gmail.com>
2025-12-02 12:59:22 -0500

    feat: comment out unused route imports and middleware in App class

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import userRoutes from '#src/routes/userRoutes.js'
-import roleRoutes from '#src/routes/roleRoutes.js'
-import authRoutes from '#src/routes/authRoutes.js'
-import DatesRoutes from '#src/routes/DatesRoutes.js'
-import clinicalHistoryRoutes from '#src/routes/clinicalHistoryRoutes.js'
-import treatmentRoutes from '#src/routes/treatmentRoutes.js'
-import { InitializeDatabase, down } from '#src/config/turso.config.js'
+// import roleRoutes from '#src/routes/roleRoutes.js'
+// import authRoutes from '#src/routes/authRoutes.js'
+// import DatesRoutes from '#src/routes/DatesRoutes.js'
+// import clinicalHistoryRoutes from '#src/routes/clinicalHistoryRoutes.js'
+// import treatmentRoutes from '#src/routes/treatmentRoutes.js'
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
@@ -46,12 +45,12 @@ export class App {
             res.send("<h1 style='h1 {margin: auto}'>Usuario confirmado correctamente, ahora puedes cerrar esta pestaña e iniciar sesión.</h1>")
         })
         this.app.get('/health', (req, res) => { res.status(200).send({ success:true, message: 'Servidor funcionando correctamente', timestamp: new Date().toISOString() }) })
-        this.app.use(`${this.apiRoute}/dates`, DatesRoutes)
         this.app.use(`${this.apiRoute}/users`, userRoutes)
-        this.app.use(`${this.apiRoute}/roles`, roleRoutes)
-        this.app.use(`${this.apiRoute}/auth`, authRoutes)
-        this.app.use(`${this.apiRoute}/clinical`, clinicalHistoryRoutes)
-        this.app.use(`${this.apiRoute}/treatment`, treatmentRoutes)
+        // this.app.use(`${this.apiRoute}/dates`, DatesRoutes)
+        // this.app.use(`${this.apiRoute}/roles`, roleRoutes)
+        // this.app.use(`${this.apiRoute}/auth`, authRoutes)
+        // this.app.use(`${this.apiRoute}/clinical`, clinicalHistoryRoutes)
+        // this.app.use(`${this.apiRoute}/treatment`, treatmentRoutes)
     }
 
     errorHandler() {
@@ -62,7 +61,6 @@ export class App {
     }
 
     async listen() {
-        // await InitializeDatabase()
         this.app.listen(this.port, '0.0.0.0', () => {
             console.log(`🚀 Servidor corriendo en http://localhost:${this.port}`)
             console.log(`📊 Health check: http://localhost:${this.port}/health`)
