650f2a21543dfa08a5a19835d3ad5137c4b0f54f
mat0wa3 <mmateo1804@gmail.com>
2025-12-04 19:58:08 -0500

    fix: correct formatting in breed and pet schemas

--- src/db/Schemas/breedSchema.js
+++ src/db/Schemas/breedSchema.js
@@ -1,7 +1,8 @@
 import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
 import { specie } from "./specieSchema.js";
+
 export const breed = sqliteTable("breed", {
-  breedID:integer ("breedID").primaryKey(),
+  breedID: integer("breedID").primaryKey(),
   specieID: integer("specieID").notNull().references(() => specie.specieID),
   name: text("name").notNull(),
   createdAt: integer("createdAt").$defaultFn(() => Date.now()),
